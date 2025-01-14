fb9c89d92fbaf36f419d9a29dce2797c0bc4be24
N1k1f0rM <nikifortch53@gmail.com>
2025-01-15 01:28:59 +0300

    prepare to deploy for realtime debug

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 from dotenv import load_dotenv
+from dataclasses import dataclass
 import os
 
+
 load_dotenv()
+
+
+@dataclass
+class Secrets:
+    BOT_TOKEN: str = os.environ.get("BOT_TOKEN")
+    WEATHER_TOKEN: str = os.environ.get("API_WEATHER_TOKEN")
+    FOOD_TOKEN: str = os.environ.get("API_FOOD_TOKEN")
+
 
 TOKEN = os.getenv("BOT_TOKEN")
 
